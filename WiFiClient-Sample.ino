5f9b8716fbacac286124fc1342043c71e4ab36a9
lgdelacruz <lgdelacruz@lgdelacruz.com>
2021-06-02 17:49:44 -0400

    Working but laggy

--- WiFiClient-Sample.ino
+++ WiFiClient-Sample.ino
@@ -21,10 +21,10 @@ void setup() {
 
   // We start by connecting to a WiFi network
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  // Serial.println();
+  // Serial.println();
+  // Serial.print("Connecting to ");
+  // Serial.println(ssid);
 
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
@@ -34,31 +34,31 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    // Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  // Serial.println("");
+  // Serial.println("WiFi connected");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
+  // Serial.print("connecting to ");
+  // Serial.print(host);
+  // Serial.print(':');
+  // Serial.println(port);
 
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   if (!client.connect(host, port)) {
-    Serial.println("connection failed");
+    // Serial.println("connection failed");
     delay(5000);
     return;
   }
 
   // This will send a string to the server
-  Serial.println("sending data to server");
+  // Serial.println("sending data to server");
   if (client.connected()) {
     client.println("GET /api/timezone/America/New_York HTTP/1.1");
     client.println();
@@ -68,7 +68,8 @@ void loop() {
   unsigned long timeout = millis();
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
+      // Serial.println(">>> Client Timeout !");
+      Serial.println("failed");
       client.stop();
       delay(10000);
       return;
@@ -76,7 +77,7 @@ void loop() {
   }
 
   // Read all the lines of the reply from server and print them to Serial
-  Serial.println("receiving from remote server");
+  // Serial.println("receiving from remote server");
   // not testing 'client.connected()' since we do not need to send data here
   String result = "";
   while (client.available()) {
@@ -89,8 +90,8 @@ void loop() {
   Serial.println(date);
 
   // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
+  // Serial.println();
+  // Serial.println("closing connection");
   client.stop();
 
   delay(10000); // execute once every 5 minutes, don't flood remote service
